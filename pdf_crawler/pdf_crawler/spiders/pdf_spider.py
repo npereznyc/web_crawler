f4fb4a885bef18469d8d4bb336b43a7b3e3115c3
npereznyc <natalieperez84@gmail.com>
2023-04-05 15:23:20 -0400

    pdf files downloading

--- pdf_crawler/pdf_crawler/spiders/pdf_spider.py
+++ pdf_crawler/pdf_crawler/spiders/pdf_spider.py
@@ -12,58 +12,57 @@ class PdfSpider(scrapy.Spider):
     download_folder = "pdf_downloads" 
 
     def parse(self, response):
-        content_type = response.headers.get('Content-Type')
-
-        if b'text/html' in content_type:
-            # Process HTML content
-            link_extractor = LinkExtractor()
-            links = link_extractor.extract_links(response)
-            for link in links:
-                yield response.follow(link.url, self.parse)
-
-        elif b'application/pdf' in content_type:
-            # Process PDF content
-            file_url = response.url
-            file_extension = os.path.splitext(file_url)[-1].lower()
-            if file_extension == '.pdf':
-                file_name = file_url.split("/")[-1]
-                file_path = os.path.join("pdfs", file_name)
-                self.logger.info(f"Saving PDF: {file_name}")
-                if not os.path.exists("pdfs"):
-                    os.makedirs("pdfs")
-                with open(file_path, "wb") as f:
-                    f.write(response.body)
+        pdf_links = response.css("a[href$='.pdf']::attr(href)").getall()
+        for pdf_link in pdf_links:
+            yield scrapy.Request(
+                response.urljoin(pdf_link),
+                self.save_pdf,
+                meta={"file_name": pdf_link.split("/")[-1]}
+            )
+        if pdf_links:
+            logging.info(f"Found {len(pdf_links)} PDF links on {response.url}")
         else:
-            self.logger.info(f"Skipping non-text, non-pdf content: {response.url}")
+            logging.info(f"No PDF links found on {response.url}")
 
-        # pdf_links = response.css("a[href$='.pdf']::attr(href)").getall()
-        # for pdf_link in pdf_links:
-        #     yield scrapy.Request(
-        #         response.urljoin(pdf_link),
-        #         self.save_pdf,
-        #         meta={"file_name": pdf_link.split("/")[-1]}
-        #     )
-        # if pdf_links:
-        #     logging.info(f"Found {len(pdf_links)} PDF links on {response.url}")
+        # Follow other links
+        start_url_domain = urlparse(self.start_urls[0]).netloc
+        other_links = response.css("a[href]::attr(href)").getall()
+
+        for link in other_links:
+            link_parsed = urlparse(link)
+            link_domain = link_parsed.netloc
+            link_scheme = link_parsed.scheme
+
+            absolute_link = response.urljoin(link)  # Convert the relative link to an absolute link
+            absolute_link_parsed = urlparse(absolute_link)
+            absolute_link_domain = absolute_link_parsed.netloc
+            absolute_link_scheme = absolute_link_parsed.scheme
+
+            if (not absolute_link_domain or absolute_link_domain == start_url_domain) and absolute_link_scheme in ['http', 'https']:
+                yield response.follow(absolute_link, self.parse)
+        # content_type = response.headers.get('Content-Type')
+
+        # if b'text/html' in content_type:
+        #     # Process HTML content
+        #     link_extractor = LinkExtractor()
+        #     links = link_extractor.extract_links(response)
+        #     for link in links:
+        #         yield response.follow(link.url, self.parse)
+
+        # elif b'application/pdf' in content_type:
+        #     # Process PDF content
+        #     file_url = response.url
+        #     file_extension = os.path.splitext(file_url)[-1].lower()
+        #     if file_extension == '.pdf':
+        #         file_name = file_url.split("/")[-1]
+        #         file_path = os.path.join("pdfs", file_name)
+        #         self.logger.info(f"Saving PDF: {file_name}")
+        #         if not os.path.exists("pdfs"):
+        #             os.makedirs("pdfs")
+        #         with open(file_path, "wb") as f:
+        #             f.write(response.body)
         # else:
-        #     logging.info(f"No PDF links found on {response.url}")
-
-        # # Follow other links
-        # start_url_domain = urlparse(self.start_urls[0]).netloc
-        # other_links = response.css("a[href]::attr(href)").getall()
-
-        # for link in other_links:
-        #     link_parsed = urlparse(link)
-        #     link_domain = link_parsed.netloc
-        #     link_scheme = link_parsed.scheme
-
-        #     absolute_link = response.urljoin(link)  # Convert the relative link to an absolute link
-        #     absolute_link_parsed = urlparse(absolute_link)
-        #     absolute_link_domain = absolute_link_parsed.netloc
-        #     absolute_link_scheme = absolute_link_parsed.scheme
-
-        #     if (not absolute_link_domain or absolute_link_domain == start_url_domain) and absolute_link_scheme in ['http', 'https']:
-        #         yield response.follow(absolute_link, self.parse)
+        #     self.logger.info(f"Skipping non-text, non-pdf content: {response.url}")
 
     def save_pdf(self, response):
         if not os.path.exists(self.download_folder):
